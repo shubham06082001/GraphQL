42c8e2dec9459f61515e95d8a45b9c69812df54a
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2023-08-02 14:29:07 +0530

    Added

--- server/index.js
+++ server/index.js
@@ -1,2 +1,16 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
+const express = require("express")
+require("dotenv").config()
+
+const { graphqlHTTP } = require("express-graphql")
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+)
+
+app.listen(port, console.log(`Server listening on port ${port}`))
