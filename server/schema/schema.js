ff51a6df618e0f42d642b8fe306d0392f323c988
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2023-08-15 17:34:42 +0530

    updated

---
+++ server/schema/schema.js
@@ -0,0 +1,41 @@
+const { projects, clients } = require("../SampleData")
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+} = require("graphql")
+
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects.filter((project) => project.clientId === parent.id)
+      },
+    },
+  }),
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id)
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+})
