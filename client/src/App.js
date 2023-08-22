ceb58116b3a97d817c0259f3058f343e2b55f8d3
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2023-08-22 10:34:43 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { useQuery, gql } from "@apollo/client"
+import Clients from "./components/Clients"
+import AddClientModal from "./components/AddClientModal"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <div className="container">
+        <AddClientModal />
+        <Clients />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+      },
+    },
+  },
+})
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache,
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
-);
+  </ApolloProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
